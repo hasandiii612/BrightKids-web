06587636f74d300ff2f8cfc8dad8199d643fd06e
hasandiii612 <hasandi.20232331@iit.ac.lk>
2025-03-05 15:55:23 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter basename="/BrightKids-web/">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </StrictMode>,
